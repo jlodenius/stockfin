09fe7991dcca75c9e770f34d67b8893020d95943
jacob <jacoblodenius@gmail.com>
2026-01-19 09:29:16 +0100

    clean up some imports

--- src/persistence.rs
+++ src/persistence.rs
@@ -1,26 +1,29 @@
 use directories::ProjectDirs;
-use std::{fs, path::PathBuf};
+use std::{
+    fs::{create_dir_all, read_to_string, write},
+    path::PathBuf,
+};
 
 pub fn get_config_path() -> PathBuf {
     let proj_dirs = ProjectDirs::from("org", "jlodenius", "stockfin")
         .expect("Could not determine config directory");
 
     let config_dir = proj_dirs.config_dir();
-    fs::create_dir_all(config_dir).ok();
+    create_dir_all(config_dir).ok();
     config_dir.join("tickers.json")
 }
 
 pub fn save_tickers(tickers: Vec<(String, String)>) {
     let path = get_config_path();
     if let Ok(json) = serde_json::to_string(&tickers) {
-        fs::write(path, json).ok();
+        write(path, json).ok();
     }
 }
 
 pub fn load_tickers() -> Vec<(String, String)> {
     let path = get_config_path();
 
-    fs::read_to_string(path)
+    read_to_string(path)
         .ok()
         .and_then(|data| serde_json::from_str::<Vec<(String, String)>>(&data).ok())
         .unwrap_or_default()
